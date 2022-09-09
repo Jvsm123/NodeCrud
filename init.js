8db009509b648b18f881d3bb7549d16912d764ec
jvsm123 <jvsm123@outlook.com>
2022-09-09 20:41:02 -0300

    making modifications

--- init.js
+++ init.js
@@ -1,100 +1,102 @@
-import ora from 'ora';
-import listr from 'listr';
-import shell from 'shelljs';
-import inquirer from 'inquirer';
+import ora from "ora"
+import listr from "listr"
+import shell from "shelljs"
+import inquirer from "inquirer"
 
-const init = () =>
-{
-	inquirer.prompt(
-	[
-		{
-			type: "list",
-			name: "manager",
-			message: "Qual gerênciador de pacotes gostária de usar?",
-			choices: [ "Npm", "Yarn" ]
-		},
-		{
-			type: "list",
-			name: "environment",
-			message: "Como deseja executar?",
-			choices: [ "Docker", "Nativo" ]
-		}
-	])
-	.then( ( options )  =>
-	{
-		const tasks = [];
+const prompt = () =>
+  inquirer.prompt([
+    {
+      type: "list",
+      name: "manager",
+      message: "Qual gerênciador de pacotes gostária de usar?",
+      choices: ["Npm", "Yarn"],
+    },
+    {
+      type: "list",
+      name: "environment",
+      message: "Como deseja executar?",
+      choices: ["Docker", "Nativo"],
+    },
+  ])
 
-		const spin = ora();
+const init = () => {
+  prompt().then((options) => {
+    const tasks = []
 
-		const { manager, environment } = options;
+    const spin = ora()
 
-		( manager === "Npm" ) && tasks.push(
-		{
-			title: "Instalando dependências",
-			task: () =>
-			{
-				spin.text = "Configurando Backend";
-				spin.start();
+    const { manager, environment } = options
 
-				shell.cd("./src/Back");
-				shell.exec("npm install");
+    try {
+      if (manager === "Npm")
+        tasks.push({
+          title: "Instalando dependências",
+          task: () => {
+            spin.text = "Configurando Backend"
+            spin.start()
 
-				spin.stop();
-				spin.text = "Configurando Frontend";
-				spin.start();
+            shell.cd("./src/Back")
+            shell.exec("npm install")
 
-				shell.cd("-");
-				shell.cd("./src/Front");
-				shell.exec("npm install");
+            spin.stop()
+            spin.text = "Configurando Frontend"
+            spin.start()
 
-				shell.cd("..");
-				shell.cd("..");
+            shell.cd("-")
+            shell.cd("./src/Front")
+            shell.exec("npm install")
 
-				spin.stop();
-			}
-		})
+            shell.cd("..")
+            shell.cd("..")
 
-		||
+            spin.stop()
+          },
+        })
+      else
+        tasks.push({
+          title: "Instalando dependências",
+          task: () => {
+            spin.text = "Configurando Backend"
+            spin.start()
 
-		tasks.push(
-		{
-			title: "Instalando dependências",
-			task: () =>
-			{
-				spin.text = "Configurando Backend";
-				spin.start();
+            shell.cd("./src/Back")
+            shell.exec("\nyarn install")
 
-				shell.cd("./src/Back");
-				shell.exec("\nyarn install");
+            spin.stop()
 
-				spin.stop();
+            spin.text = "Configurando Frontend"
+            spin.start()
 
-				spin.text = "Configurando Frontend";
-				spin.start();
+            shell.cd("-")
+            shell.cd("./src/Front")
+            shell.exec("\nyarn install")
 
-				shell.cd("-");
-				shell.cd("./src/Front")
-				shell.exec("\nyarn install");
+            shell.cd("..")
+            shell.cd("..")
 
-				shell.cd("..");
-				shell.cd("..");
+            spin.stop()
+          },
+        })
+    } catch (err) {
+      throw new Error("erro ao fazer operação: ", err)
+    }
 
-				spin.stop();
-			}
-		});
+    new listr(tasks).run().then(() => {
+      try {
+        environment === "Nativo" &&
+          shell.echo(
+            `para rodar, basta digitar '${manager} run app' na raiz do projeto ;)`
+          )
 
-		new listr(tasks)
-			.run()
-			.then( () =>
-			{
-				(environment === "Nativo")
-					&& shell.echo("para rodar, basta digitar 'npm run app' na raiz do projeto ;)");
-
-				(environment === "Docker")
-					&& shell.echo("Para rodar, basta digitar 'npm run docker' na raiz do projeto :)");
-			})
-			.catch( err => console.error("houve um problema aqui: ", err) );
-	});
+        environment === "Docker" &&
+          shell.echo(
+            `Para rodar, basta digitar '${manager} run docker' na raiz do projeto :)`
+          )
+      } catch (err) {
+        throw new Error("Erro ao lançar sistema: ", err)
+      }
+    })
+  })
 }
 
-init();
+init()
